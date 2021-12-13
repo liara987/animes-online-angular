88fb87efb664f971593de697b145993c549d3157
Liara Pereira Duarte <987.liara@gmail.com>
2021-12-13 02:21:36 -0300

    feature: Componente List

---
+++ src/app/list/list.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { CardType } from '../card/card.component';
+import animes from '../animes.json';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.scss'],
+})
+export class ListComponent implements OnInit {
+  cards: CardType[] = [];
+
+  ngOnInit(): void {
+    this.setCards()
+  }
+
+  setCards() {
+    animes.forEach((anime, i) => {
+      this.cards[i] = {
+        id: anime.id,
+        title: anime.title,
+        image: anime.cover,
+      };
+    });
+  }
+}
