be1bb5629851c7c00c9ad691f9f1f78fc65be7e2
liara987 <987.liara@gmail.com>
2022-03-14 01:29:12 -0300

    feature: Adiciona storie do Navbar no Storybook

---
+++ src/app/nav-bar/nav-bar.component.stories.ts
@@ -0,0 +1,10 @@
+import { Meta, Story } from '@storybook/angular';
+import { NavBarComponent } from './nav-bar.component'
+
+export default {
+    title: 'NavBar',
+    component: NavBarComponent,
+  } as Meta;
+  
+  export const Default: Story = () => ({
+  });
