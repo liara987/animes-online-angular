4ffb80dacb65f1ab3f4a6aa9c065807c849dc31c
LIARA PEREIRA DUARTE <987.liara@gmail.com>
2023-08-30 00:30:03 -0300

    chore: Remove console

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -17,13 +17,11 @@ export class CarouselComponent implements OnInit {
   onPreviousClick() {
     const previous = this.currentSlide - 1;
     this.currentSlide = previous < 0 ? this.slides.length - 1 : previous;
-    console.log("previous clicked, new current slide is: ", this.currentSlide);
   }
 
   onNextClick() {
     const next = this.currentSlide + 1;
     this.currentSlide = next === this.slides.length ? 0 : next;
-    console.log("next clicked, new current slide is: ", this.currentSlide);
   }
 
 }
